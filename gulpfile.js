e903e639ef41e456558f6ebcc189150ef67068a7
zhanghj <1058776714@qq.com>
2018-10-17 17:09:24 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var pngcrush = require('imagemin-pngcrush');
+var minifycss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
+var autoprefixer = require('gulp-autoprefixer');
+var rem = require('gulp-rem-plus');
+var px2rem = require('gulp-px2rem-plugin');
+
+//编译sass文件
+gulp.task('sass', function(){
+  return gulp.src('src/sass/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('src/css/'))
+})
+
+// 合并、压缩、重命名css
+gulp.task('css', function() {
+	return gulp.src('src/css/*.css')
+		.pipe(px2rem())
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(concat('all.css'))
+		.pipe(gulp.dest('dist/css'))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(minifycss())
+		.pipe(gulp.dest('dist/css'))
+		.pipe(notify({
+			message: 'css task ok'
+		}));
+});
+
+// 合并、压缩js文件
+gulp.task('js', function() {
+	return gulp.src('src/js/*.js')
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('dist/js'))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'))
+		.pipe(notify({
+			message: 'js task ok'
+		}));
+});
+
+// 压缩图片
+gulp.task('img', function() {
+	return gulp.src('src/images/*')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{
+				removeViewBox: false
+			}],
+			use: [pngcrush()]
+		}))
+		.pipe(gulp.dest('dist/images'))
+		.pipe(notify({
+			message: 'img task ok'
+		}));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('src/sass/*scss', ['sass']);
+	gulp.watch('src/css/*.css', ['css']);
+	gulp.watch('src/js/*.js', ['js']);
+	gulp.watch('src/images/*', ['img']);
+});
+
+// 默认任务
+gulp.task('default', function() {
+  gulp.run('sass', 'css', 'js', 'img', 'watch');
+});
